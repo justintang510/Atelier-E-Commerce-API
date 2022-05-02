ac2f84bf6c25887bc9096b2ccb58e6a60f766897
Justin Tang <97769405+justintang510@users.noreply.github.com>
2022-05-02 09:31:39 -0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const db = new Pool({
-  user: 'process.env.USER',
+  user: process.env.USER,
   host: process.env.HOST,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
